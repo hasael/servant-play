22a75181fa405993f117e54a49d1d3360ae6dfe7
hasael <elmanhasa@gmail.com>
2021-04-05 22:05:31 +0100

    Add app startup config

--- app/Main.hs
+++ app/Main.hs
@@ -12,25 +12,37 @@ import Lib
 import RealDb
 import InMemoryDb
 import Models
-
-mainReal :: IO ()
-mainReal = do
-  config <- readConfig
-  connectionsPool <- initConnection $ (connectionString . db) config
-  state <- newState
-  initDb connectionsPool
-  startAligner connectionsPool state
-  concurrently_ (startApp 8080 $ app connectionsPool state) (scheduled connectionsPool state 4000000)
+import System.Environment
 
 main :: IO ()
 main = do
+  args <- getArgs
+  print $ "Args: " ++ show args
   state <- newState
+  let initArg = if not $ null args then Just $ head args else Nothing 
+  print initArg
+  case initArg of
+    Just "local" -> startReal "./local-config.yaml" state
+    Just "dev" -> startReal "./dev-config.yaml" state
+    _ -> startInMemory state
+
+startInMemory :: AppState -> IO ()
+startInMemory state = do
   db <- newDB 
   startAligner db state
   concurrently_ (startApp 8080 $ app db state) (scheduled db state 4000000)
 
+
+startReal :: FilePath -> AppState -> IO ()
+startReal config state = do
+  config <- readConfig config
+  connectionsPool <- initConnection $ (connectionString . db) config
+  initDb connectionsPool
+  startAligner connectionsPool state
+  concurrently_ (startApp 8080 $ app connectionsPool state) (scheduled connectionsPool state 4000000)
+
 scheduled :: (DbRepository IO a) => a -> AppState -> Int -> IO ()
 scheduled conn state delay = threadDelay delay >> merge_ conn state >> scheduled conn state delay
 
-readConfig :: IO AppConfig
-readConfig = decodeFileThrow "./local-config.yaml"
+readConfig :: FilePath -> IO AppConfig
+readConfig = decodeFileThrow
