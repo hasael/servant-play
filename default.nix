105c5e7cd02e24b0347c9b414c0ca0b002f1adec
hasael <elmanhasa@gmail.com>
2021-05-08 20:08:53 +0100

    Update nix file

--- default.nix
+++ default.nix
@@ -1,27 +1,29 @@
 { mkDerivation, aeson, async, base, bytestring, containers, Decimal
-, either, generic-random, hspec, hspec-wai, hspec-wai-json
-, http-types, lib, postgresql-simple, QuickCheck, refined
+, either, envy, generic-random, hspec, hspec-wai, hspec-wai-json
+, http-types, lib, mtl, postgresql-simple, QuickCheck, refined
 , resource-pool, servant-server, stm, text, transformers, wai
 , wai-extra, warp, yaml
 }:
 mkDerivation {
   pname = "servant-play";
-  version = "0.1.0.2";
+  version = "0.1.0.4";
   src = ./.;
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
-    aeson base bytestring containers Decimal either postgresql-simple
-    refined resource-pool servant-server stm text transformers wai
-    wai-extra warp
+    aeson base bytestring containers Decimal either mtl
+    postgresql-simple refined resource-pool servant-server stm text
+    transformers wai wai-extra warp
   ];
   executableHaskellDepends = [
-    aeson async base postgresql-simple servant-server wai warp yaml
+    aeson async base envy postgresql-simple servant-server wai warp
+    yaml
   ];
   testHaskellDepends = [
     aeson async base bytestring containers generic-random hspec
-    hspec-wai hspec-wai-json http-types postgresql-simple QuickCheck
-    refined resource-pool servant-server stm wai wai-extra warp
+    hspec-wai hspec-wai-json http-types mtl postgresql-simple
+    QuickCheck refined resource-pool servant-server stm wai wai-extra
+    warp
   ];
   homepage = "https://github.com/hasael/servant-play#readme";
   license = lib.licenses.bsd3;
